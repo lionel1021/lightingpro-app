20d293d41a814c77f6e9ae6cbb5928d316434265
lionel1021 <zhanglixin1999913@gmail.com>
2025-07-11 08:22:31 +0800

    🔥 CRITICAL FIX: Remove conflicting i18n.js file
    - Delete i18n.js that was referencing wrong message path (./messages/ instead of ./src/messages/)
    - This file was conflicting with proper src/i18n/request.ts configuration
    - Should finally fix language routing so /de loads German content not Chinese

--- next-intl.config.js
+++ next-intl.config.js
@@ -1,7 +1,7 @@
 import { createSharedPathnamesNavigation } from 'next-intl/navigation'
 
 export const defaultLocale = 'en'
-export const locales = ['en', 'zh']
+export const locales = ['en', 'zh', 'fr', 'es', 'de', 'it']
 
 export const { Link, redirect, usePathname, useRouter } = createSharedPathnamesNavigation({
   locales,
